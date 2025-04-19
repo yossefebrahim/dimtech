bbd9a233c9037a60c98de67b33cc4fe9598abd14
yossefebrahim <59574378+yossefebrahim@users.noreply.github.com>
2025-04-19 17:58:38 +0200

    update num of dots

--- app/_components/ParticleBackground.tsx
+++ app/_components/ParticleBackground.tsx
@@ -29,7 +29,7 @@ const ParticleBackground = () => {
 
           generateTextPositions("");
 
-          let totalParticles = 6000;
+          let totalParticles = 10000;
           for (let i = 0; i < totalParticles; i++) {
             let particle = new Particle(p.random(p.width), p.random(p.height));
             if (i < textPositions.length) {
